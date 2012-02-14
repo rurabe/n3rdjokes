b21dcebb25d7490252e2cc574a48bd4f937b14b4
Ryan Urabe <rurabe@gmail.com>
2012-02-14 09:51:24 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+N3rdjokes::Application.config.secret_token = '06e811c84b8fdfcb633a30cf5e98554cc2ad598e4e1bc0a4809e5282903398cd0cf2721319e192578faf7d038d05c716e555d5a857d13d5161b2881d0ba8be3f'
